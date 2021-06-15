59b078b3adfe617201dbeef810dc663b8492b59c
jela3105 <jela3105@gmail.com>
2021-06-14 20:09:21 -0500

    Basic example of useEffect

--- src/componets/02-useEffect/SimpleForm.js
+++ src/componets/02-useEffect/SimpleForm.js
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./effects.css";
 
 export const SimpleForm = () => {
+  const [formState, setFormState] = useState({ name: "", email: "" });
+
+  const { name, email } = formState;
+  useEffect(() => {
+    console.log("hey");
+  }, []);
+
+  useEffect(() => {
+    console.log("Name changed");
+  }, [name]);
+
+  useEffect(() => {
+    console.log("Email changed");
+  }, [email]);
+
+  const handleInputChange = ({ target }) => {
+    setFormState({
+      ...formState,
+      [target.name]: target.value,
+    });
+  };
+
   return (
     <>
       <h1>useEffect</h1>
       <hr />
+      <div className="form-group">
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="Your name"
+          autoComplete="off"
+          value={name}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="text"
+          name="email"
+          className="form-control"
+          placeholder="Your email"
+          autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
+        />
+      </div>
     </>
   );
 };
