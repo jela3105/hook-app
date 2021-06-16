275767f60bc13789245e28e6fff3d43ace8164e6
jela3105 <jela3105@gmail.com>
2021-06-15 22:07:08 -0500

    Warnings in useEffect

--- src/componets/02-useEffect/Message.js
+++ src/componets/02-useEffect/Message.js
@@ -1,15 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const { x, y } = coords;
+
   useEffect(() => {
-    console.log("mount component");
+    const mouseMove = (e) => {
+      const coords = { x: e.x, y: e.y };
+      setCoords(coords);
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
     return () => {
-      console.log("unmount component");
+      window.removeEventListener("mousemove", mouseMove);
     };
   }, []);
   return (
     <div>
       <h3> You are genial </h3>
+      <p>
+        x:{x}, y:{y}
+      </p>
     </div>
   );
 };
