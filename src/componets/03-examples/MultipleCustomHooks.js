ef3db64c3be2810c6a03c72452ca1f980418575c
jela3105 <jela3105@gmail.com>
2021-06-18 21:22:27 -0500

    Show quote received from api

--- src/componets/03-examples/MultipleCustomHooks.js
+++ src/componets/03-examples/MultipleCustomHooks.js
@@ -3,11 +3,26 @@ import "../02-useEffect/effects.css";
 import { useFetch } from "../../hooks/useFetch";
 
 export const MultipleCustomHooks = () => {
-  const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-  console.log(state);
+  const { loading, data } = useFetch(
+    `https://www.breakingbadapi.com/api/quotes/1`
+  );
+  const { author, quote } = !!data && data[0];
+
   return (
     <div>
-      <h1>Multiple Custom Hooks</h1>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+      {loading && (
+        <div className="alert alert-info text-center">Loading...</div>
+      )}
+      {!loading && (
+        <blockquote className="text-right">
+          <p> {quote} </p>
+          <footer className="blockquote-footer">{author}</footer>
+        </blockquote>
+      )}
+
+      <button className="btn btn-primary">Next quote</button>
     </div>
   );
 };
