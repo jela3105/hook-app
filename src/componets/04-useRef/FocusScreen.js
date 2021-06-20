4b66632d1231f93ad6f15eeba4a5337394b22780
jela3105 <jela3105@gmail.com>
2021-06-19 21:53:03 -0500

    Basic use of useRef

--- src/componets/04-useRef/FocusScreen.js
+++ src/componets/04-useRef/FocusScreen.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../02-useEffect/effects.css";
 
 export const FocusScreen = () => {
+  const inputRef = useRef();
   const handeClick = () => {
-    document.querySelector("input").select();
+    inputRef.current.select();
   };
   return (
     <div>
       <h1>FocusScreen</h1>
       <hr />
-      <input type="text" className="form-control" placeholder="Name" />
+      <input
+        ref={inputRef}
+        type="text"
+        className="form-control"
+        placeholder="Name"
+      />
       <button className="btn btn-outline-primary mt-5" onClick={handeClick}>
         Focus
       </button>
