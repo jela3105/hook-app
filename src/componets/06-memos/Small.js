a3832ebf2f360508c3f6fcc4bf600d4b748113ff
jela3105 <jela3105@gmail.com>
2021-06-21 14:04:31 -0500

    Render component only when properties change

--- src/componets/06-memos/Small.js
+++ src/componets/06-memos/Small.js
@@ -1,5 +1,6 @@
 import React from "react";
 
-export const Small = ({ value }) => {
+export const Small = React.memo(({ value }) => {
+  console.log("Change component");
   return <small>{value}</small>;
-};
+});
