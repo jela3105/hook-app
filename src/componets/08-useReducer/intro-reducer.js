4058c47dc52c438fcfbc9ec0db11e80bc9f93ade
jela3105 <jela3105@gmail.com>
2021-06-27 22:23:59 -0500

    Change state in reducer to add todo

--- src/componets/08-useReducer/intro-reducer.js
+++ src/componets/08-useReducer/intro-reducer.js
@@ -7,9 +7,25 @@ const initialState = [
 ];
 
 const todoReducer = (state = initialState, action) => {
+  if (action?.type === "create") {
+    return [...state, action.payload];
+  }
   return state;
 };
 
 let todos = todoReducer();
+
+const newTodo = {
+  id: 2,
+  todo: "Buy milk",
+  done: false,
+};
+
+const createTodoAction = {
+  type: "create",
+  payload: newTodo,
+};
+
+todos = todoReducer(todos, createTodoAction);
 
 console.log(todos);
