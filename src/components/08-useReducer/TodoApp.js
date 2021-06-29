79a62ee2b4da78ec241efb9c26c1410164281c16
jela3105 <jela3105@gmail.com>
2021-06-28 23:35:35 -0500

    Create todoReducer

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { todoReducer } from "./todoReducer";
+
 import "./styles.css";
 
+const initialState = [
+  { id: new Date().getTime(), desc: "Learn react", done: false },
+];
+
 export const TodoApp = () => {
+  const [todos] = useReducer(todoReducer, initialState);
+  console.log(todos);
   return (
     <div>
       <h1>TodoApp</h1>
       <hr />
+      <ul>
+        <li>Hello</li>
+        <li>World</li>
+        <li>Todo</li>
+      </ul>
     </div>
   );
 };
---
+++ src/components/08-useReducer/todoReducer.js
@@ -0,0 +1,9 @@
+const todoReducer = (state = [], action) => {
+  switch (action.type) {
+    case "add":
+      break;
+
+    default:
+      return state;
+  }
+};
