2919fb2ad820b2952c3e3b1c51233bbee7d32c47
jela3105 <jela3105@gmail.com>
2021-06-29 23:38:00 -0500

    Add new todo

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -8,8 +8,24 @@ const initialState = [
 ];
 
 export const TodoApp = () => {
-  const [todos] = useReducer(todoReducer, initialState);
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
   console.log(todos);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: "New task",
+      done: false,
+    };
+
+    const action = {
+      type: "add",
+      payload: newTodo,
+    };
+
+    dispatch(action);
+  };
   return (
     <div>
       <h1>TodoApp ({todos.length})</h1>
@@ -20,7 +36,7 @@ export const TodoApp = () => {
             {todos.map((todo, i) => (
               <li key={todo.id} className="list-group-item">
                 <p className="text-center">
-                  {i + 1}.{todo.desc}
+                  {i + 1}. {todo.desc}
                 </p>
                 <button className="btn btn-danger">Delete</button>
               </li>
@@ -29,16 +45,14 @@ export const TodoApp = () => {
         </div>
         <div className="col-5">
           <h4>Add todo</h4>
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <input
               type="text"
               className="form-control"
               name="description"
               placeholder="Learn..."
             />
-            <button className="btn btn-outline-primary mt-1 btn-block">
-              Add
-            </button>
+            <button className="btn btn-outline-primary mt-1">Add</button>
           </form>
           <hr />
         </div>
