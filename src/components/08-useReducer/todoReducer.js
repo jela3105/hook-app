25b73f61ef0fc57ed56074ccb3e435d5ad3dcc65
jela3105 <jela3105@gmail.com>
2021-07-01 23:29:24 -0500

    Using ternary operator

--- src/components/08-useReducer/todoReducer.js
+++ src/components/08-useReducer/todoReducer.js
@@ -5,13 +5,9 @@ export const todoReducer = (state = [], action) => {
     case "remove":
       return state.filter((todo) => todo.id !== action.payload);
     case "toggle":
-      return state.map((todo) => {
-        if (todo.id === action.payload) {
-          return { ...todo, done: !todo.done };
-        } else {
-          return todo;
-        }
-      });
+      return state.map((todo) =>
+        todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+      );
 
     default:
       return state;
