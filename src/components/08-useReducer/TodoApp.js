c651687b45d4c4e5398a40de14f0f08b9a9eb47f
jela3105 <jela3105@gmail.com>
2021-07-01 23:26:37 -0500

    Mark todo as completed (toggle)

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -29,6 +29,10 @@ export const TodoApp = () => {
     dispatch(action);
   };
 
+  const handleToggle = (todoId) => {
+    dispatch({ type: "toggle", payload: todoId });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -59,7 +63,10 @@ export const TodoApp = () => {
           <ul className="list=group list-group-flush">
             {todos.map((todo, i) => (
               <li key={todo.id} className="list-group-item">
-                <p className="text-center">
+                <p
+                  className={`${todo.done && "complete"}`}
+                  onClick={() => handleToggle(todo.id)}
+                >
                   {i + 1}. {todo.desc}
                 </p>
                 <button
