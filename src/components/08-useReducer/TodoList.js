38a79de04a0585d0f22efb0d446660ad95055a87
jela3105 <jela3105@gmail.com>
2021-07-02 23:29:52 -0500

    Create TodoListItem

--- src/components/08-useReducer/TodoList.js
+++ src/components/08-useReducer/TodoList.js
@@ -1,24 +1,17 @@
 import React from "react";
+import { TodoListItem } from "./TodoListItem";
 
 export const TodoList = ({ todos, handleToggle, handleDelete }) => {
   return (
     <div className="col-7">
       <ul className="list=group list-group-flush">
         {todos.map((todo, i) => (
-          <li key={todo.id} className="list-group-item">
-            <p
-              className={`${todo.done && "complete"}`}
-              onClick={() => handleToggle(todo.id)}
-            >
-              {i + 1}. {todo.desc}
-            </p>
-            <button
-              className="btn btn-danger"
-              onClick={() => handleDelete(todo.id)}
-            >
-              Delete
-            </button>
-          </li>
+          <TodoListItem
+            todo={todo}
+            i={i}
+            handleDelete={handleDelete}
+            handleToggle={handleToggle}
+          />
         ))}
       </ul>
     </div>
