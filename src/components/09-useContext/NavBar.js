0999c8d38e21bbd332238da62bab2b01f51c52c5
jela3105 <jela3105@gmail.com>
2021-07-06 23:27:41 -0500

    Add styles to NavBar

--- src/components/09-useContext/NavBar.js
+++ src/components/09-useContext/NavBar.js
@@ -3,18 +3,23 @@ import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="./">Home</Link>
-        </li>
-        <li>
-          <Link to="./about">About</Link>
-        </li>
-        <li>
-          <Link to="./login">Login</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">
+        useContext
+      </Link>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <Link className="nav-link active" aria-current="page" to="/">
+            Home
+          </Link>
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+          <Link className="nav-link" to="/login">
+            Pricing
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 };
