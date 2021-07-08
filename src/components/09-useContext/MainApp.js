27364f8150ff881008575fe2edc4481d0eae91ee
jela3105 <jela3105@gmail.com>
2021-07-07 21:51:33 -0500

    Create context with useContext

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { AppRouter } from "./AppRouter";
+import { UserContext } from "./UserContext";
 
 export const MainApp = () => {
-  return <AppRouter />;
+  const user = { id: 1234, name: "Jela", email: "jela@jela.com" };
+  return (
+    <UserContext.Provider value={user}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
 };
